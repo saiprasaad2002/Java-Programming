9567fd8a33840aadc1bb7560fdc9fb73883f60f2
saiprasaad2002 <saiprasaad18@gmail.com>
2025-05-09 15:22:18 +0530

    Changed folder structure

---
+++ src/Animal.java
@@ -0,0 +1,20 @@
+//Inheritance and
+
+public class Animal {
+    String name;
+    int age;
+    public Animal(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void eat(){
+        System.out.println(name+" is eating");
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+}
---
+++ src/Dog.java
@@ -0,0 +1,25 @@
+public class Dog extends Animal{
+    String breed;
+    public Dog(String name,int age,String breed){
+        super(name,age);
+        this.breed = breed;
+    }
+
+    public void bark(){
+        System.out.println(name+" is barking!!");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
+
+    public String getBreed(){
+        return breed;
+    }
+    public static void main(String[] args){
+        Dog dog = new Dog("Tommy",7,"German Shepherd");
+        dog.eat();
+
+    }
+}
